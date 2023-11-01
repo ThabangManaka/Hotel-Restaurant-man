a09c324713b03fac278ab2e782622b56aaf0570f
ThabangManaka <thabangdansteyn@gmail.com>
2023-11-01 16:06:10 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hotel-restuarant-man',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
